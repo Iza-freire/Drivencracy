43233bf3490b1f7cfc3851f818570efa3b0b4a0a
IzaFreire <kariny.frei.2000@gmail.com>
2023-02-01 15:47:35 -0300

    Reestruturação

--- src/index.js
+++ src/index.js
@@ -1,63 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { MongoClient, ObjectId } from 'mongodb';
 
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 const port = process.env.PORT || 5008;
 
-const url = process.env.MONGO_URL;
-const client = new MongoClient(url, { useUnifiedTopology: true });
 
-async function startServer() {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB");
-        const db = client.db('polls');
-        const pollsCollection = db.collection('polls');
-        const choicesCollection = db.collection('choices');
+const app = express();
 
-        app.post('/poll', async (req, res) => {
-            const { title, expireAt = '' } = req.body;
-            if (!title) {
-                return res.status(422).send({ error: 'Title is required' });
-            }
-
-            const now = new Date();
-            const defaultExpireAt = new Date(now.getTime() + 30 * 24 * 60 * 60 * 1000);
-            const parsedExpireAt = expireAt ? new Date(expireAt) : defaultExpireAt;
-
-            const poll = {
-                title,
-                expireAt: parsedExpireAt.toISOString().slice(0, 16),
-            };
-
-            const { insertedId } = await pollsCollection.insertOne(poll);
-            return res.status(201).send({
-                _id: insertedId,
-                title,
-                expireAt: parsedExpireAt.toISOString().slice(0, 16),
-            });
-        });
-
-        app.get('/poll', async (req, res) => {
-            const polls = await pollsCollection.find({}).toArray();
-            return res.send(polls);
-        });
+app.use(express.json());
+app.use(cors());
 
 
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
 
-startServer();
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+
 
 

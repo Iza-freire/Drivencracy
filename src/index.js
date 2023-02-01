d9998aa9f36655b2a4a121e0e99a615d75a1b654
IzaFreire <kariny.frei.2000@gmail.com>
2023-02-01 12:35:53 -0300

    Implemtando requisitos da rota /poll

--- src/index.js
+++ src/index.js
@@ -21,6 +21,29 @@ async function startServer() {
         const pollsCollection = db.collection('polls');
         const choicesCollection = db.collection('choices');
 
+        app.post('/poll', async (req, res) => {
+            const { title, expireAt = '' } = req.body;
+            if (!title) {
+                return res.status(422).send({ error: 'Title is required' });
+            }
+
+            const now = new Date();
+            const defaultExpireAt = new Date(now.getTime() + 30 * 24 * 60 * 60 * 1000);
+            const parsedExpireAt = expireAt ? new Date(expireAt) : defaultExpireAt;
+
+            const poll = {
+                title,
+                expireAt: parsedExpireAt.toISOString().slice(0, 16),
+            };
+
+            const { insertedId } = await pollsCollection.insertOne(poll);
+            return res.status(201).send({
+                _id: insertedId,
+                title,
+                expireAt: parsedExpireAt.toISOString().slice(0, 16),
+            });
+        });
+
         app.listen(port, () => {
             console.log(`Server running on port ${port}`);
         });
